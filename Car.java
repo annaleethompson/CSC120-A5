a9e638ff0728950de08b05adfac57ea5e1302461
annaleethompson <acthompson@smith.edu>
2023-03-03 20:50:58 -0500

    Day 1

--- Car.java
+++ Car.java
@@ -1,3 +1,92 @@
-public class Car {
+import java.util.ArrayList;
+import javax.management.RuntimeErrorException;
 
+
+
+
+public class Car {
+    private ArrayList<Passenger> riders;
+    int max_capacity;
+    int current_capacity = 0;
+ 
+    
+
+    public Car( int max_capacity, int current_capacity) {
+        this.current_capacity = current_capacity;
+        this.riders = new ArrayList<Passenger>();
+        this.max_capacity = max_capacity;
+    }
+
+    public int getCapacity() {
+        return this.max_capacity;
+    }
+
+    public int seatsRemaining() {
+        return this.max_capacity-this.current_capacity;
+    }
+    public void addPassenger(Passenger p) {
+        if (this.riders.contains(p)) { // Already enrolled
+            throw new RuntimeException(p.name + " is already on this car.");
+        }
+        if (current_capacity == max_capacity) {
+            throw new RuntimeException("Sorry! This car is at maximum capacity.");
+        }
+        
+        this.riders.add();
+        this.current_capacity +=1;
+        System.out.println(p.name + " was successfully added to the car.");
+    }
+
+    public void removePassenger(Passenger p) {
+        if (!this.riders.contains(p)) {
+            throw new RuntimeException("Sorry! This passeenger is not on this car.");
+        }
+        if (current_capacity == 0) {
+            throw new RuntimeException("Sorry! There are no passengers in this car.");
+        }
+        this.riders.remove(p);
+        this.current_capacity -=1;
+        System.out.println(p.name + " was successfully removed from the car.");
+    } 
+    
+    public void printManifest() {
+        for (int i=0; i < riders.size(); i++) {
+        //System.out.println(riders.toString(riders.name));
+            System.out.println((i.toString()))
+        
+        
+        }
+    
+
+    
+    public static void main(String[] args) {
+        Car myCar = new Car(100, 0);
+        Passenger p = new Passenger("Egg");
+        //System.out.print(myCar.getCapacity());
+        //System.out.println("\n");
+        //System.out.print(myCar.seatsRemaining());
+        //System.out.println("\n");
+        myCar.addPassenger(p);
+        //myCar.addPassenger(p);
+        //System.out.println("\n");
+        //System.out.print(myCar.seatsRemaining());
+        //System.out.println("\n");
+        //myCar.removePassenger(p);
+        //System.out.println("\n");
+        //System.out.print(myCar.seatsRemaining());
+        Passenger s = new Passenger("Eggy");
+        myCar.addPassenger(s);
+        myCar.printManifest();
+
+
+    }   
 }
+
+//## Step 2: the `Car` class
+//Next, we'll set to work on the `Car` class. The `Car` class will need:
+
+ //- a private `ArrayList` where it will store the `Passenger`s currently onboard, and an `int` for the `Car`'s maximum capacity (since `ArrayList`s will expand as we add objects, we'll need to manually limit their size)
+ // -   - a constructor, which takes in an initial value for the `Car`'s maximum capacity and initializes an appropriately-sized `ArrayList`
+ //- accessor-like methods `public int getCapacity()` and `public int seatsRemaining()` that return the maximum capacity and remaining seats, respectively
+ //- methods `public void addPassenger(Passenger p)` and `public void removePassenger(Passenger p)` to add or remove a `Passenger` from the `Car` (_Hint: don't forget to check that there are seats available if someone wants to board, and to confirm that the `Passenger` is actually onboard before trying to remove them! If you encounter a problem, throw a `RuntimeException`._)
+ //- and a final method `public void printManifest()` that prints out a list of all `Passenger`s aboard the car (or "This car is EMPTY." if there is no one on board)
--- Engine.java
+++ Engine.java
@@ -1,3 +1,47 @@
 public class Engine {
+    private String FuelType;
+    double max_fuel;
+    double current_fuel;
 
+    public Engine(String FuelType,  double current_fuel) {
+        this.FuelType = FuelType;
+        this.max_fuel = 100;
+        this.current_fuel = current_fuel;
+
+    }
+
+    public void refuel() {
+        if (this.max_fuel == this.current_fuel) { 
+            throw new RuntimeException("Engine is already at maximum fuel: " + this.max_fuel);
+        }
+        this.current_fuel = 10;
+        System.out.println("Engine was successfully refueled! Engine now at fuel level: " + this.current_fuel);
+    }
+    
+    public void go() {
+        if (this.current_fuel < 1) {
+            throw new RuntimeException("Engine doesn't have enough fuel for this trip. Remaining fuel level is " + this.current_fuel);
+        }
+        this.current_fuel -= 10;
+        System.out.println("Trip was succesful! Remaining fuel level: " +this.current_fuel);
+    }
+
+    public static void main(String[] args) {
+        Engine myEngine = new Engine("ELECTRIC", 100.0);
+        try {
+            while (true) {
+                myEngine.go();
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage()); // Out of fuel
+        }
+    }
 }
+
+//## Step 1: The `Engine` class
+// We recommend you start by implementing the `Engine` class. Your `Engine` class will need:
+ 
+ //- a private `FuelType` attribute to indicate what type of fuel it uses, and `double`s to store the current and maximum fuel levels (along with an approproate accessors for each)
+ //- a constructor, which takes in initial values for the attributes named above and sets them appropriately
+ //- a method `public void refuel()` which will reset the `Engine`'s current fuel level to the maximum
+ //- a method `public void go()` which will decrease the current fuel level and print some useful information (e.g. remaining fuel level) provided the fuel level is above 0 (otherwise it should throw a `RuntimeException` containing an informative message)
